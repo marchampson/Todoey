2100d8d59115cdf76f214d400eb91b455fb071f4
Marc Hampson <mhampson@wk360.com>
2017-12-22 09:28:18 +0000

    Convert to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Marc Hampson on 22/12/2017.
+//  Copyright © 2017 Marc Hampson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
